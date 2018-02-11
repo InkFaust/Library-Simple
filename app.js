c2c4b563c6e068e0372e1e0d9fc0d6b0485c100b
InkFaust <inkfaust@gmail.com>
2018-02-11 02:39:21 +0200

    Heroku

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ if (!isDBExist)
     });
 
 var app = (module.exports = express());
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 4000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "client/build")));
